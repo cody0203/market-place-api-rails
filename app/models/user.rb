fae05cf7c3f6a737f2cab4749a0ad0752d3cc08f
Pham Cong Dinh <congdinh2395@gmail.com>
2021-05-24 20:11:04 +0700

    Setup bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password_digest, presence: true
+
+  has_secure_password
 end
